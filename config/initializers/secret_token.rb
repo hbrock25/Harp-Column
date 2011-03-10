cb7fd43d5cf9430d6883be7ecd0670894ffae271
Hugh Brock <hbrock@redhat.com>
2011-03-10 08:27:47 -0500

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harpcolumn::Application.config.secret_token = '79d395653acd6cb55307e44bc9ae9a6b5d315cd9e6a0ee832797f93548650f2abe09d6ca7924152daff8f9ebe498e64522ffe9b31c80e1ec519d864c951acd42'
