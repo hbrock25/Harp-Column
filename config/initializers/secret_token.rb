22bb541b5022a3bb3080d44bde15b3c797983b31
John Metta <john@johnmetta.com>
2011-07-25 15:09:51 -0700

    Vanilla rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harpcolumn::Application.config.secret_token = 'd712fa929bb2e78be4322a262c02b9004871258b7f452760d0dc83dba5165eb1774840871ca9c19bb6f87d66e9f69375cab4a862e45ce9ae5582d0e8a3cef343'
