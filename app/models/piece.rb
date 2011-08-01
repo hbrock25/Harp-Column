ed5df2547bec422c51ee55cdc56d5d35693c1115
seth-macpherson <seth.macpherson@gmail.com>
2011-08-01 15:50:57 -0700

    fixed the pieces.rb merge bork

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,10 +3,6 @@ class Piece < ActiveRecord::Base
   acts_as_votable
   rails_admin
 
-<<<<<<< HEAD
-  has_and_belongs_to_many   :performances
-
-=======
   # we needed to create the join table for pieces to instruments for this to work because it's a "has_and_belongs_to_many"
   has_and_belongs_to_many :performances
   has_many  :pictures, :as    => :imageable
@@ -24,5 +20,4 @@ class Piece < ActiveRecord::Base
     5
   end
   
->>>>>>> 03434f3995e8f3d1f3f685ecd2656ed2d019405e
 end
