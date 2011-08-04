29cd87d3b95a1689dfbc1f80580fbe17cf251222
John Metta <john@johnmetta.com>
2011-08-04 14:59:09 -0700

    Stub out user tagging in sound clips

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Harpcolumn::Application.routes.draw do
 
   resources :pieces
   resources :pictures
+  # TODO: This stub functionality was to test the backend- we need better RESTful routing
   match "/pictures/:id/add_user/:user_slug" => "pictures#add_user"
   match "/pictures/:id/del_user/:user_slug" => "pictures#del_user"
   resources :sound_clips
+  match "/sound_clips/:id/add_user/:user_slug" => "sound_clips#add_user"
+  match "/sound_clips/:id/del_user/:user_slug" => "sound_clips#del_user"
   resources :venues
   devise_for :users
   resources :users
